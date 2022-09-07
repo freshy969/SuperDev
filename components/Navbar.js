06c7fdac9d4b6a1661694567803db990915dff0c
Choudhary Abdullah <magnetonx@gmail.com>
2022-09-08 02:19:26 +0530

    Added ContentJS

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,4 @@
 import {useEffect} from 'react';
-import ReactDOM from 'react-dom';
 
 export default function Navbar() {
 	useEffect(() => {
---
+++ js/content.js
@@ -0,0 +1,6 @@
+(() => {
+	// If Popup Exists, Remove Everything on Extension Icon Click
+	if (document.getElementById('superDev') !== null) {
+		document.getElementById('superDev').remove();
+	}
+})();
