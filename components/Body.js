970a1449b81fb35428de87c4c7f6b8a481f5d6a1
Choudhary Abdullah <magnetonx@gmail.com>
2022-09-08 17:56:53 +0530

    Add/Remove Fully Working (Fixing Height Now)

--- components/Body.js
+++ components/Body.js
@@ -6,9 +6,14 @@ export default function Body() {
 	const [isLoading, setIsLoading] = useState(true);
 	useEffect(() => {
 		//Add All Features to LocalStorage
-		localStorage.setItem('enabledFeatures', JSON.stringify(features));
-		console.log('All Features Added to LocalStorage : ', localStorage.getItem('enabledFeatures'));
-		setIsLoading(false);
+		if (localStorage.getItem('enabledFeatures') === null) {
+			localStorage.setItem('enabledFeatures', JSON.stringify(features));
+			console.log('All Features Added to LocalStorage : ', localStorage.getItem('enabledFeatures'));
+			setIsLoading(false);
+		} else {
+			console.log('The Existent One, If Any : ', localStorage.getItem('enabledFeatures'));
+			setIsLoading(false);
+		}
 	}, []);
 
 	// Set enabledFeatures as soon as useEffect finishes execution
@@ -19,6 +24,7 @@ export default function Body() {
 			if (value.id === featureId) {
 				if (value.isEnabled === false) {
 					console.log('Found Disabled Feature : ', featureId);
+					console.log(document.getElementsByClassName(featureId));
 					document.getElementsByClassName(featureId)[0].classList.remove('hidden');
 					document.getElementsByClassName(featureId)[1].classList.replace(value.addIcon, value.closeIcon);
 					value.isEnabled = true;
@@ -41,32 +47,58 @@ export default function Body() {
 					<section id='mainBody' className='w-[340px] h-[498px]'>
 						<div className='grid grid-cols-2 gap-x-[14px] p-4 pb-0 border border-borderLight dark:border-borderDark box-border rounded-b-lg'>
 							{enabledFeatures.map((value, index) => {
-								return (
-									<button
-										key={index}
-										id={value.id}
-										className={
-											value.id +
-											' rounded-md text-left bg-gradient-to-r from-btnOne to-btnTwo shadow-lg text-xs text-bodyText p-2 mb-4 font-normal transition ease-in-out hover:scale-[1.03] duration-300'
-										}>
-										<i className={'fa-regular ' + value.icon + ' px-[5px] text-bodyText'}></i> {value.title}
-									</button>
-								);
+								if (value.isEnabled !== false) {
+									return (
+										<button
+											key={index}
+											id={value.id}
+											className={
+												value.id +
+												' rounded-md text-left bg-gradient-to-r from-btnOne to-btnTwo shadow-lg text-xs text-bodyText p-2 mb-4 font-normal transition ease-in-out hover:scale-[1.03] duration-300'
+											}>
+											<i className={'fa-regular ' + value.icon + ' px-[5px] text-bodyText'}></i> {value.title}
+										</button>
+									);
+								} else {
+									return (
+										<button
+											key={index}
+											id={value.id}
+											className={
+												value.id +
+												' rounded-md text-left bg-gradient-to-r from-btnOne to-btnTwo shadow-lg text-xs text-bodyText p-2 mb-4 font-normal transition ease-in-out hover:scale-[1.03] duration-300 hidden'
+											}>
+											<i className={'fa-regular ' + value.icon + ' px-[5px] text-bodyText'}></i> {value.title}
+										</button>
+									);
+								}
 							})}
 						</div>
 					</section>
 					<section id='hideFeature' className='hidden w-[340px] h-[498px]'>
 						<div className='grid grid-cols-2 gap-x-[14px] p-4 pb-0 border border-borderLight dark:border-borderDark box-border rounded-b-lg'>
 							{enabledFeatures.map((value, index) => {
-								return (
-									<button
-										key={index}
-										id={value.id}
-										onClick={() => hideFeature(value.id)}
-										className='rounded-md text-left bg-gradient-to-r from-btnOne to-btnTwo shadow-lg text-xs text-bodyText p-2 mb-4 font-normal transition ease-in-out hover:scale-[1.03] duration-300'>
-										<i className={value.id + ' fa-solid ' + value.closeIcon + ' px-[7px] text-bodyText text-[10px]'}></i> {value.title}
-									</button>
-								);
+								if (value.isEnabled !== false) {
+									return (
+										<button
+											key={index}
+											id={value.id}
+											onClick={() => hideFeature(value.id)}
+											className='rounded-md text-left bg-gradient-to-r from-btnOne to-btnTwo shadow-lg text-xs text-bodyText p-2 mb-4 font-normal transition ease-in-out hover:scale-[1.03] duration-300'>
+											<i className={value.id + ' fa-solid ' + value.closeIcon + ' px-[7px] text-bodyText text-[10px]'}></i> {value.title}
+										</button>
+									);
+								} else {
+									return (
+										<button
+											key={index}
+											id={value.id}
+											onClick={() => hideFeature(value.id)}
+											className='rounded-md text-left bg-gradient-to-r from-btnOne to-btnTwo shadow-lg text-xs text-bodyText p-2 mb-4 font-normal transition ease-in-out hover:scale-[1.03] duration-300'>
+											<i className={value.id + ' fa-solid ' + value.addIcon + ' px-[7px] text-bodyText text-[10px]'}></i> {value.title}
+										</button>
+									);
+								}
 							})}
 						</div>
 					</section>
