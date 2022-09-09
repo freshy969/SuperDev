e1deabcf39a2e74d2bda50274b97f2048bc271a5
Choudhary Abdullah <magnetonx@gmail.com>
2022-09-09 18:50:14 +0530

    Calculate Small MainBody Height?

--- components/Navbar.js
+++ components/Navbar.js
@@ -47,25 +47,23 @@ export default function Navbar() {
 
 	function hideFeature() {
 		// Show/hide Main/Edit Popup and Features
-
 		console.log(new Date().getSeconds(), new Date().getMilliseconds(), 'Hide/Show Icon Clicked, NavbarJs');
-
 		if (document.getElementById('mainBody').classList.contains('hidden')) {
-			document.getElementById('mainBody').classList.remove('hidden');
-			document.getElementById('hideFeature').classList.add('hidden');
-			console.log(new Date().getSeconds(), new Date().getMilliseconds(), 'Showing Hidden Main Body, NavbarJs');
 			chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
 				chrome.tabs.sendMessage(tabs[0].id, {message: 'changeHeight', height: document.getElementById('mainBody').offsetHeight + 40.5}, function (response) {
 					console.log(new Date().getSeconds(), new Date().getMilliseconds(), 'NavbarJs Received from ContentJS', response.farewell);
+					document.getElementById('mainBody').classList.remove('hidden');
+					document.getElementById('hideFeature').classList.add('hidden');
+					console.log(new Date().getSeconds(), new Date().getMilliseconds(), 'Showing Hidden Main Body, NavbarJs');
 				});
 			});
 		} else {
-			console.log(new Date().getSeconds(), new Date().getMilliseconds(), 'Hiding Existent Main Body, NavbarJs');
-			document.getElementById('mainBody').classList.add('hidden');
-			document.getElementById('hideFeature').classList.remove('hidden');
 			chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
 				chrome.tabs.sendMessage(tabs[0].id, {message: 'changeHeight', height: 538.5}, function (response) {
 					console.log(new Date().getSeconds(), new Date().getMilliseconds(), 'NavbarJs Received from ContentJS', response.farewell);
+					document.getElementById('mainBody').classList.add('hidden');
+					document.getElementById('hideFeature').classList.remove('hidden');
+					console.log(new Date().getSeconds(), new Date().getMilliseconds(), 'Hiding Existent Main Body, NavbarJs');
 				});
 			});
 		}
