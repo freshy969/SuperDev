218f1adb003e3957f21b2db5fb55021441593721
Choudhary Abdullah <magnetonx@gmail.com>
2022-10-20 20:09:44 +0530

    Update features.js

--- data/features.js
+++ data/features.js
@@ -46,61 +46,66 @@ const features = [
 			checkboxColorPickerTwo: 'unchecked',
 		},
 	},
-	{
-		title: 'Color Palette',
-		icon: 'fa-swatchbook',
-		id: 'colorPalette',
-	},
-	{
-		title: 'CSS Copier',
-		icon: 'fa-copy',
-		id: 'cssCopier',
-	},
-	{
-		title: 'Custom CSS',
-		icon: 'fa-code',
-		id: 'customCSS',
-	},
-	{
-		title: 'Custom JS',
-		icon: 'fa-brackets-curly',
-		id: 'customJS',
-	},
-	{
-		title: 'Font Changer',
-		icon: 'fa-font-case',
-		id: 'fontChanger',
-	},
-	{
-		title: 'Export Element',
-		icon: 'fa-up-right-from-square',
-		id: 'exportElement',
-	},
-	{
-		title: 'Extract Media',
-		icon: 'fa-image',
-		id: 'extractMedia',
-	},
-	{
-		title: 'Take Screenshot',
-		icon: 'fa-aperture',
-		id: 'takeScreenshot',
-	},
-	{
-		title: 'View Responsive',
-		icon: 'fa-laptop-mobile',
-		id: 'viewResponsive',
-	},
-	{
-		title: 'Clear Cache',
-		icon: 'fa-recycle',
-		id: 'clearCache',
-	},
-	{
-		title: 'Go Incognito',
-		icon: 'fa-mask',
-		id: 'goIncognito',
-	},
+	// {
+	// 	title: 'Color Palette',
+	// 	icon: 'fa-swatchbook',
+	// 	id: 'colorPalette',
+	// },
+	// {
+	// 	title: 'CSS Copier',
+	// 	icon: 'fa-copy',
+	// 	id: 'cssCopier',
+	// },
+	// {
+	// 	title: 'Custom CSS',
+	// 	icon: 'fa-code',
+	// 	id: 'customCSS',
+	// },
+	// {
+	// 	title: 'Custom JS',
+	// 	icon: 'fa-brackets-curly',
+	// 	id: 'customJS',
+	// },
+	// {
+	// 	title: 'Font Changer',
+	// 	icon: 'fa-font-case',
+	// 	id: 'fontChanger',
+	// },
+	// {
+	// 	title: 'Export Element',
+	// 	icon: 'fa-up-right-from-square',
+	// 	id: 'exportElement',
+	// },
+	// {
+	// 	title: 'Extract Media',
+	// 	icon: 'fa-image',
+	// 	id: 'extractMedia',
+	// },
+	// {
+	// 	title: 'Take Screenshot',
+	// 	icon: 'fa-aperture',
+	// 	id: 'takeScreenshot',
+	// },
+	// {
+	// 	title: 'View Responsive',
+	// 	icon: 'fa-laptop-mobile',
+	// 	id: 'viewResponsive',
+	// },
+	// {
+	// 	title: 'Clear Cache',
+	// 	icon: 'fa-recycle',
+	// 	id: 'clearCache',
+	// },
+	// {
+	// 	title: 'Go Incognito',
+	// 	icon: 'fa-mask',
+	// 	id: 'goIncognito',
+	// },
+	// {
+	// 	title: 'Built With',
+	// 	icon: 'fa-server',
+	// 	id: 'builtWith',
+	// },
 ];
 
 export default features;
