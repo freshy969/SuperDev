9d05042f5976f10c9515931b3385a4f11d6ffb49
Choudhary Abdullah <magnetonx@gmail.com>
2022-10-22 18:08:24 +0530

    Test

--- src/components/functions/ChangeHeight.js
+++
@@ -1,9 +0,0 @@
-export default function ChangeHeight(height) {
-	chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-		let portFour = chrome.tabs.connect(tabs[0].id, {name: 'portFour'});
-		portFour.postMessage({action: 'changeHeight', height: height});
-		portFour.onMessage.addListener((response) => {
-			console.log('Got Response : ', response.action);
-		});
-	});
-}
--- src/components/functions/JustChangeHeight.js
+++
@@ -1,9 +0,0 @@
-export default function JustChangeHeight(height) {
-	chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-		let portFour = chrome.tabs.connect(tabs[0].id, {name: 'portFour'});
-		portFour.postMessage({action: 'justChangeHeight', height: height});
-		portFour.onMessage.addListener((response) => {
-			console.log('Got Response : ', response.action);
-		});
-	});
-}
