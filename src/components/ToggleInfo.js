3a7404d43bfe1bb7d13dd757fac4b80e30a81784
Choudhary Abdullah <magnetonx@gmail.com>
2022-10-26 14:29:45 +0530

    Update ToggleInfo.js

--- src/components/ToggleInfo.js
+++ src/components/ToggleInfo.js
@@ -6,7 +6,25 @@ export default function ToggleInfo() {
 				<div id='toggleInfoChild' className='rounded-md p-4'>
 					<div>
 						<div className='rounded-md text-left bg-bgTwo border border-borderTwo shadow-lg text-xs text-allText p-2 mb-3 font-normal select-none'>
-							<i className='fa-regular fa-up-right-from-square px-[5px] text-allText'></i>Extension Shortcuts
+							<i className='fa-regular fa-command px-[5px] text-allText'></i>Extension Shortcuts
+						</div>
+						<div className='rounded-md border bg-bgTwo border-borderTwo shadow-lg p-3 mb-3'>
+							{[`Open Extension : ${window.navigator?.userAgentData?.platform === 'macOS' ? 'Cmd+Shift+S' : 'Ctrl+Shift+S'}`].map(function (value, index) {
+								return (
+									<div className={index + 1 === 1 ? '' : 'mt-2'} key={index + 1}>
+										<h6 className='inline-block text-xs text-allText font-normal select-none' htmlFor={'checkboxExportElement' + (index + 1)}>
+											<i className='fa-regular fa-square-info pr-[5px] text-allText'></i>
+											{value}
+										</h6>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+
+					<div>
+						<div className='rounded-md text-left bg-bgTwo border border-borderTwo shadow-lg text-xs text-allText p-2 mb-3 font-normal select-none'>
+							<i className='fa-regular fa-up-right-from-square px-[5px] text-allText'></i>About Export Element
 						</div>
 						<div className='rounded-md border bg-bgTwo border-borderTwo shadow-lg p-3 mb-3'>
 							{[
