58e8e2721ce57f20ad1835e442f94318e0b3c1c5
Choudhary Abdullah <magnetonx@gmail.com>
2022-11-03 23:05:31 +0530

    Optimised HomeJs

--- src/Home.js
+++ src/Home.js
@@ -18,16 +18,16 @@ export default function Home() {
 
 	useEffect(function () {
 		// Initialisation/Reset on First Load
-		chrome.storage.local.set({isStopBtnPressed: false});
-		chrome.storage.local.set({setHomePageActive: false});
-		chrome.storage.local.set({setActFeatDisabled: false});
-		chrome.storage.local.set({setMinimised: null});
-		chrome.storage.local.set({whichFeatureActive: null});
-		chrome.storage.local.set({howLongPopupIs: null});
+		chrome.storage.local.set({isStopBtnPressed: false}); // True, False
+		chrome.storage.local.set({setHomePageActive: false}); // True, False
+		chrome.storage.local.set({setActFeatDisabled: false}); // True, False
+		chrome.storage.local.set({setMinimised: null}); // True, False, Null
+		chrome.storage.local.set({whichFeatureActive: null}); // String, Null
+		chrome.storage.local.set({howLongPopupIs: null}); // Number, Null
 
 		// Dark Mode Initialisation
 		chrome.storage.local.get(['colorTheme'], function (result) {
-			if (result.colorTheme === undefined) {
+			if (!result.colorTheme) {
 				if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
 					document.documentElement.classList.add('dark');
 					chrome.storage.local.set({colorTheme: 'dark'});
@@ -36,7 +36,6 @@ export default function Home() {
 					// Dark Theme as Default
 					chrome.storage.local.set({colorTheme: 'dark'});
 					document.documentElement.classList.add('dark');
-
 					setIsLoadingOne(false);
 				}
 			} else if (result.colorTheme === 'dark') {
@@ -49,8 +48,10 @@ export default function Home() {
 
 		// Set AllFeatures
 		chrome.storage.local.get(['allFeatures'], function (result) {
-			setAllFeatures(JSON.parse(result.allFeatures));
-			setIsLoadingTwo(false);
+			if (result.allFeatures) {
+				setAllFeatures(JSON.parse(result.allFeatures));
+				setIsLoadingTwo(false);
+			}
 		});
 
 		// OnUpdate AllFeatures
