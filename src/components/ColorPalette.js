dc56a8f0a994d54bf77607c218165f298443306a
Choudhary Abdullah <magnetonx@gmail.com>
2022-11-04 00:35:14 +0530

    Optimised ColorPaletteJs

--- src/components/ColorPalette.js
+++ src/components/ColorPalette.js
@@ -50,49 +50,46 @@ export default function ColorPalette({allFeatures, portThree}) {
 			<div className='border border-t-0 border-borderOne dark:border-borderOneD box-border rounded-b-lg'>
 				<div id='colorPalettePageChild' className='rounded-md p-4'>
 					<div className='flex flex-wrap'>
-						{!isLoadingOne && allColors.length !== 0
-							? allColors.map(function (value, index) {
-									// Margin Right/Bottom Calculation
-									let id = 'colorPalette' + (index + 1);
-									let allClasses;
-									let baseClasses =
-										'flex flex-wrap justify-center items-center h-[25.5px] w-[90px] rounded-md box-border border border-gray-300 cursor-pointer ';
-									if (allColors.length - (index + 1) >= 3) {
-										if (((index + 1) / 3) % 1 !== 0) allClasses = baseClasses + 'mr-[14.5px] mb-[14.5px]';
-										else allClasses = baseClasses + 'mb-[14.5px]';
-									} else {
-										if (((index + 1) / 3) % 1 !== 0) allClasses = baseClasses + 'mr-[14.5px]';
-										else allClasses = baseClasses + '';
-									}
+						{!isLoadingOne &&
+							allColors.length !== 0 &&
+							allColors.map(function (value, index) {
+								// Margin Right/Bottom Calculation
+								let id = 'colorPalette' + (index + 1);
+								let allClasses;
+								let baseClasses = 'flex flex-wrap justify-center items-center h-[25.5px] w-[90px] rounded-md box-border border border-gray-300 cursor-pointer ';
+								if (allColors.length - (index + 1) >= 3) {
+									if (((index + 1) / 3) % 1 !== 0) allClasses = baseClasses + 'mr-[14.5px] mb-[14.5px]';
+									else allClasses = baseClasses + 'mb-[14.5px]';
+								} else {
+									if (((index + 1) / 3) % 1 !== 0) allClasses = baseClasses + 'mr-[14.5px]';
+									else allClasses = baseClasses + '';
+								}
 
-									// TickIcon Color Calculation
-									let intelColor = value.startsWith('#')
-										? hexToRgb(value).split('(')[1].split(')')[0].split(',')
-										: value.split('(')[1].split(')')[0].split(',');
-									if (intelColor[0] * 0.299 + intelColor[1] * 0.587 + intelColor[2] * 0.114 > 150) intelColor = 'black';
-									else intelColor = 'white';
+								// TickIcon Color Calculation
+								let intelColor = value.startsWith('#') ? hexToRgb(value).split('(')[1].split(')')[0].split(',') : value.split('(')[1].split(')')[0].split(',');
+								if (intelColor[0] * 0.299 + intelColor[1] * 0.587 + intelColor[2] * 0.114 > 150) intelColor = 'black';
+								else intelColor = 'white';
 
-									return (
-										<div
-											id={id}
-											onClick={function () {
-												copyColorCode(value, id);
-											}}
-											onMouseEnter={function () {
-												showColorCode(value, id);
-											}}
-											onMouseLeave={function () {
-												hideColorCode(value, id);
-											}}
-											className={allClasses}
-											style={{'background-color': value}}
-											key={index + 1}>
-											<i className='fa-solid fa-badge-check text-sm before:hidden' style={{color: intelColor}}></i>
-											<span className='text-[10px]' style={{color: intelColor}}></span>
-										</div>
-									);
-							  })
-							: ''}
+								return (
+									<div
+										id={id}
+										onClick={function () {
+											copyColorCode(value, id);
+										}}
+										onMouseEnter={function () {
+											showColorCode(value, id);
+										}}
+										onMouseLeave={function () {
+											hideColorCode(value, id);
+										}}
+										className={allClasses}
+										style={{'background-color': value}}
+										key={index + 1}>
+										<i className='fa-solid fa-badge-check text-sm before:hidden' style={{color: intelColor}}></i>
+										<span className='text-[10px]' style={{color: intelColor}}></span>
+									</div>
+								);
+							})}
 					</div>
 				</div>
 			</div>
