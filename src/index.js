29a7dc36c4dcbb424a829b4a8e6d57b771a83b59
Choudhary Abdullah <magnetonx@gmail.com>
2022-11-03 23:52:41 +0530

    Minor Changes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './css/global.css';
 import './libs/css/font-awesome/css/pro.min.css';
 
 const superDevBody = ReactDOM.createRoot(document.getElementById('superDevBody'));
+console.log('Page Re-Rendered');
 superDevBody.render(
 	<React.StrictMode>
 		<Home />
