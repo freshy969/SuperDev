9e9902f5760dcbbde60f666580b1827a397fb527
Choudhary Abdullah <magnetonx@gmail.com>
2022-11-09 14:14:19 +0530

    Added Info 😎

--- src/components/ToggleInfo.js
+++ src/components/ToggleInfo.js
@@ -12,13 +12,13 @@ export default function ToggleInfo({allFeatures, activeTab, portThree, allFeatur
 		<section id='toggleInfo' className='hidden'>
 			<div className='border border-t-0 border-borderOne dark:border-borderOneD box-border rounded-b-lg'>
 				<div id='toggleInfoChild' className='rounded-md p-4'>
-					{/* SuperDev Free Shortcuts */}
+					{/* Click To Change Shortcuts */}
 					<div>
 						<div
 							className='rounded-md text-left bg-bgTwo dark:bg-bgTwoD border box-border border-borderTwo dark:border-borderTwoD shadow text-xs text-allText dark:text-allTextD p-2 mb-3 font-normal select-none cursor-pointer'
 							onClick={openShortcutPage}>
 							<i className='fa-regular fa-command px-[5px] text-allText dark:text-allTextD'></i>
-							SuperDev Free Shortcuts
+							Click To Change Shortcuts
 						</div>
 						<div className='rounded-md border bg-bgTwo dark:bg-bgTwoD border-borderTwo dark:border-borderTwoD shadow p-3 mb-3'>
 							{[
@@ -27,6 +27,48 @@ export default function ToggleInfo({allFeatures, activeTab, portThree, allFeatur
 								`Close Active Feature : ${window.navigator?.userAgentData?.platform === 'macOS' ? 'Escape' : 'Escape'}`,
 								`Clear All Cache : ${window.navigator?.userAgentData?.platform === 'macOS' ? 'Cmd+Shift+E' : 'Ctrl+Shift+E'}`,
 							].map(function (value, index) {
+								return (
+									<div className={index + 1 === 1 ? '' : 'mt-2'} key={index + 1}>
+										<h6
+											className='inline-block text-xs text-allText dark:text-allTextD font-normal select-none'
+											htmlFor={'checkboxExportElement' + (index + 1)}>
+											<i className='fa-regular fa-square-info pr-[5px] text-allText dark:text-allTextD'></i>
+											{value}
+										</h6>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+
+					{/* About Page Ruler Feature */}
+					<div>
+						<div className='rounded-md text-left bg-bgTwo dark:bg-bgTwoD border box-border border-borderTwo dark:border-borderTwoD shadow text-xs text-allText dark:text-allTextD p-2 mb-3 font-normal select-none'>
+							<i className='fa-regular fa-arrows-up-down-left-right px-[5px] text-allText dark:text-allTextD'></i>About Page Ruler Feature
+						</div>
+						<div className='rounded-md border bg-bgTwo dark:bg-bgTwoD border-borderTwo dark:border-borderTwoD shadow p-3 mb-3'>
+							{["It Won't Work On Moving Elements", 'Since It Uses Screenshot For Data :('].map(function (value, index) {
+								return (
+									<div className={index + 1 === 1 ? '' : 'mt-2'} key={index + 1}>
+										<h6
+											className='inline-block text-xs text-allText dark:text-allTextD font-normal select-none'
+											htmlFor={'checkboxExportElement' + (index + 1)}>
+											<i className='fa-regular fa-square-info pr-[5px] text-allText dark:text-allTextD'></i>
+											{value}
+										</h6>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+
+					{/* About Color Picker Feature */}
+					<div>
+						<div className='rounded-md text-left bg-bgTwo dark:bg-bgTwoD border box-border border-borderTwo dark:border-borderTwoD shadow text-xs text-allText dark:text-allTextD p-2 mb-3 font-normal select-none'>
+							<i className='fa-regular fa-arrows-up-down-left-right px-[5px] text-allText dark:text-allTextD'></i>About Color Picker Feature
+						</div>
+						<div className='rounded-md border bg-bgTwo dark:bg-bgTwoD border-borderTwo dark:border-borderTwoD shadow p-3 mb-3'>
+							{["It Won't Work On Moving Elements", 'Since It Uses Screenshot For Colors :('].map(function (value, index) {
 								return (
 									<div className={index + 1 === 1 ? '' : 'mt-2'} key={index + 1}>
 										<h6
