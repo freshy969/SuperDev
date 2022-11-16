c912fad865f0253a64469cc00c9c8af5c698f171
Choudhary Abdullah <magnetonx@gmail.com>
2022-11-16 23:12:20 +0530

    Removed Move/Delete/Export Element Info

--- src/components/ToggleInfo.js
+++ src/components/ToggleInfo.js
@@ -105,69 +105,6 @@ export default function ToggleInfo({allFeatures, activeTab, portThree, allFeatur
 						</div>
 					</div>
 
-					{/* About Move Element Feature */}
-					<div>
-						<div className='rounded-md text-left bg-bgTwo dark:bg-bgTwoD border box-border border-borderTwo dark:border-borderTwoD shadow text-xs text-allText dark:text-allTextD p-2 mb-3 font-normal select-none'>
-							<i className='fa-regular fa-arrows-up-down-left-right px-[5px] text-allText dark:text-allTextD'></i>About Move Element Feature
-						</div>
-						<div className='rounded-md border bg-bgTwo dark:bg-bgTwoD border-borderTwo dark:border-borderTwoD shadow p-3 mb-3'>
-							{["It Can't Be Used On HTML BODY", 'Since This Will Reload The Extension'].map(function (value, index) {
-								return (
-									<div className={index + 1 === 1 ? '' : 'mt-2'} key={index + 1}>
-										<h6
-											className='inline-block text-xs text-allText dark:text-allTextD font-normal select-none'
-											htmlFor={'checkboxExportElement' + (index + 1)}>
-											<i className='fa-regular fa-square-info pr-[5px] text-allText dark:text-allTextD'></i>
-											{value}
-										</h6>
-									</div>
-								);
-							})}
-						</div>
-					</div>
-
-					{/* About Delete Element Feature */}
-					<div>
-						<div className='rounded-md text-left bg-bgTwo dark:bg-bgTwoD border box-border border-borderTwo dark:border-borderTwoD shadow text-xs text-allText dark:text-allTextD p-2 mb-3 font-normal select-none'>
-							<i className='fa-regular fa-trash-can px-[5px] text-allText dark:text-allTextD'></i>About Delete Element Feature
-						</div>
-						<div className='rounded-md border bg-bgTwo dark:bg-bgTwoD border-borderTwo dark:border-borderTwoD shadow p-3 mb-3'>
-							{["It Can't Be Used On HTML BODY", 'Since This Will Delete All Page Data'].map(function (value, index) {
-								return (
-									<div className={index + 1 === 1 ? '' : 'mt-2'} key={index + 1}>
-										<h6
-											className='inline-block text-xs text-allText dark:text-allTextD font-normal select-none'
-											htmlFor={'checkboxExportElement' + (index + 1)}>
-											<i className='fa-regular fa-square-info pr-[5px] text-allText dark:text-allTextD'></i>
-											{value}
-										</h6>
-									</div>
-								);
-							})}
-						</div>
-					</div>
-
-					{/* About Export Element Feature */}
-					<div>
-						<div className='rounded-md text-left bg-bgTwo dark:bg-bgTwoD border box-border border-borderTwo dark:border-borderTwoD shadow text-xs text-allText dark:text-allTextD p-2 mb-3 font-normal select-none'>
-							<i className='fa-regular fa-up-right-from-square px-[5px] text-allText dark:text-allTextD'></i>About Export Element Feature
-						</div>
-						<div className='rounded-md border bg-bgTwo dark:bg-bgTwoD border-borderTwo dark:border-borderTwoD shadow p-3 mb-3'>
-							{["Export Element's CSS Is Buggy", 'Its CSS Algorithm Is Being Fixed'].map(function (value, index) {
-								return (
-									<div className={index + 1 === 1 ? '' : 'mt-2'} key={index + 1}>
-										<h6
-											className='inline-block text-xs text-allText dark:text-allTextD font-normal select-none'
-											htmlFor={'checkboxExportElement' + (index + 1)}>
-											<i className='fa-regular fa-square-info pr-[5px] text-allText dark:text-allTextD'></i>
-											{value}
-										</h6>
-									</div>
-								);
-							})}
-						</div>
-					</div>
-
 					{/* SuperDev Pro Coming Soon */}
 					<div>
 						<div className='rounded-md text-left bg-bgTwo dark:bg-bgTwoD border box-border border-borderTwo dark:border-borderTwoD shadow text-xs text-allText dark:text-allTextD p-2 mb-3 font-normal select-none'>
