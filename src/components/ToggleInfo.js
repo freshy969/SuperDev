f930fd59c8590ea513b927441908f938f2ab1f60
Choudhary Abdullah <magnetonx@gmail.com>
2022-11-16 23:18:34 +0530

    Removed Some Info

--- src/components/ToggleInfo.js
+++ src/components/ToggleInfo.js
@@ -21,7 +21,7 @@ export default function ToggleInfo({allFeatures, activeTab, portThree, allFeatur
 							Click To Change Shortcuts
 						</div>
 						<div className='rounded-md border bg-bgTwo dark:bg-bgTwoD border-borderTwo dark:border-borderTwoD shadow p-3 mb-3'>
-							{['Some Shortcuts Might Not Work', 'Since They Already Being Used', "Change Them If That's The Case"].map(function (value, index) {
+							{['Default Shortcuts Might Not Work', "Change Them If That's The Case"].map(function (value, index) {
 								return (
 									<div className={index + 1 === 1 ? '' : 'mt-2'} key={index + 1}>
 										<h6
@@ -63,34 +63,13 @@ export default function ToggleInfo({allFeatures, activeTab, portThree, allFeatur
 						</div>
 					</div>
 
-					{/* About Page Ruler Feature */}
+					{/* About Page Ruler/Color Picker */}
 					<div>
 						<div className='rounded-md text-left bg-bgTwo dark:bg-bgTwoD border box-border border-borderTwo dark:border-borderTwoD shadow text-xs text-allText dark:text-allTextD p-2 mb-3 font-normal select-none'>
-							<i className='fa-regular fa-arrows-up-down-left-right px-[5px] text-allText dark:text-allTextD'></i>About Page Ruler Feature
+							<i className='fa-regular fa-arrows-up-down-left-right px-[5px] text-allText dark:text-allTextD'></i>About Page Ruler/Color Picker
 						</div>
 						<div className='rounded-md border bg-bgTwo dark:bg-bgTwoD border-borderTwo dark:border-borderTwoD shadow p-3 mb-3'>
 							{["It Won't Work On Moving Elements", 'Since It Uses Screenshot For Data :('].map(function (value, index) {
-								return (
-									<div className={index + 1 === 1 ? '' : 'mt-2'} key={index + 1}>
-										<h6
-											className='inline-block text-xs text-allText dark:text-allTextD font-normal select-none'
-											htmlFor={'checkboxExportElement' + (index + 1)}>
-											<i className='fa-regular fa-square-info pr-[5px] text-allText dark:text-allTextD'></i>
-											{value}
-										</h6>
-									</div>
-								);
-							})}
-						</div>
-					</div>
-
-					{/* About Color Picker Feature */}
-					<div>
-						<div className='rounded-md text-left bg-bgTwo dark:bg-bgTwoD border box-border border-borderTwo dark:border-borderTwoD shadow text-xs text-allText dark:text-allTextD p-2 mb-3 font-normal select-none'>
-							<i className='fa-regular fa-arrows-up-down-left-right px-[5px] text-allText dark:text-allTextD'></i>About Color Picker Feature
-						</div>
-						<div className='rounded-md border bg-bgTwo dark:bg-bgTwoD border-borderTwo dark:border-borderTwoD shadow p-3 mb-3'>
-							{["It Won't Work On Moving Elements", 'Since It Uses Screenshot For Colors :('].map(function (value, index) {
 								return (
 									<div className={index + 1 === 1 ? '' : 'mt-2'} key={index + 1}>
 										<h6
